55e9ded40c6f9ab7d1e4b265b64f66f60058d6e6
Laurent Laborde <kerdezixe@gmail.com>
2021-12-03 22:31:58 +0100

    playing with fp16

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ print(tf.__version__)
 
 class MandelbrotDataSet:
     def __init__(self, size=1000, max_depth=100, xmin=-2.0, xmax=0.7, ymin=-1.3, ymax=1.3):
-        self.x = tf.random.uniform((size,),xmin,xmax,tf.float32)
-        self.y = tf.random.uniform((size,),ymin,ymax,tf.float32)
+        self.x = tf.random.uniform((size,),xmin,xmax,tf.float16)
+        self.y = tf.random.uniform((size,),ymin,ymax,tf.float16)
         self.outputs = self.mandel(x=self.x, y=self.y,max_depth=max_depth)
         self.data = tf.stack([self.x, self.y], axis=1)
 
@@ -22,11 +22,11 @@ class MandelbrotDataSet:
         zx, zy = x,y
         for n in range(1, max_depth):
             zx, zy = zx*zx - zy*zy + x, 2*zx*zy + y
-        return tf.cast(tf.less(zx*zx+zy*zy, 4.0),tf.float16) #* 2.0 - 1.0
+        return tf.cast(tf.less(zx*zx+zy*zy, 4.0),tf.float16) * 2.0 - 1.0
 
 #%%
 
-trainingData = MandelbrotDataSet(100_000)
+trainingData = MandelbrotDataSet(1_000_000)
 
 #%%
 
@@ -36,18 +36,19 @@ trainingData = MandelbrotDataSet(100_000)
 
 model = tf.keras.Sequential()
 tf.keras.Input(shape=(2,)),
-for _ in range(10):
+for _ in range(20):
     model.add(tf.keras.layers.Dense(20,activation="relu"))
+model.add(tf.keras.layers.Dense(2, activation="softmax"))
 model.add(tf.keras.layers.Dense(1,activation=None))
 
 model.compile(loss=tf.keras.losses.MeanSquaredError(),
-              optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002),
 #              optimizer=tf.keras.optimizers.Adadelta(learning_rate=1.0),
               metrics=["accuracy", "mae", "mse"])
 
 #%%
 
-history = model.fit(trainingData.data, trainingData.outputs,epochs=10,batch_size=1000)
+history = model.fit(trainingData.data, trainingData.outputs,epochs=100,batch_size=10000)
 
 #%%
 
@@ -61,14 +62,15 @@ def plot_loss(history):
   plt.plot(history.history['loss'], label='loss')
   plt.xlabel('Epoch')
   plt.ylabel('Error')
+  plt.ylim((0.0, 0.1))
   plt.legend()
   plt.grid(True)
 
 plot_loss(history)
 
 #%%
-x = tf.random.uniform((200_000,), -2.0, 0.7, tf.float32)
-y = tf.random.uniform((200_000,), -1.3, 1.3, tf.float32)
+x = tf.random.uniform((200_000,), -2.0, 0.7, tf.float16)
+y = tf.random.uniform((200_000,), -1.3, 1.3, tf.float16)
 data = tf.stack([x, y], axis=1)
 predictions = model.predict(data)
 
